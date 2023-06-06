fbf7e7dece576326a681fe839ee4fdde46538082
jasmijnslot <jasmijn.slot@novi-education.nl>
2023-06-06 10:00:23 +0200

    Initial commit

---
+++ src/main/java/com/example/backenddierenarts/BackendDierenartsApplication.java
@@ -0,0 +1,13 @@
+package com.example.backenddierenarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDierenartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDierenartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backenddierenarts/BackendDierenartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backenddierenarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDierenartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
